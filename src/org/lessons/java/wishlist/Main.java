072b8f86cdcfebd34ce0edb03a37f47328a017a1
Gabriele02 <gagag@g,ail.com>
2023-12-07 14:32:55 +0100

    finish work

---
+++ src/org/lessons/java/wishlist/Main.java
@@ -0,0 +1,45 @@
+package org.lessons.java.wishlist;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        HashSet<String> gift = new HashSet<>();
+
+        boolean stop = false;
+        while(!stop){
+            System.out.println("Insert new gift?(yes/no)");
+            String answer = scanner.nextLine();
+            switch (answer){
+                case "yes":
+                    System.out.print("Name of the gift to add: ");
+                    String name = scanner.nextLine();
+                    if(!gift.contains(name)){
+                        gift.add(name);
+                        System.out.println("La tua lista dei regali contiene: " + (gift.size()) + " " + "oggetto");
+                    } else {
+                        System.out.println("Il nome del regalo esiste già nella lista");
+                    }
+                    break;
+                case "no":
+                    stop = true;
+                    break;
+                default:
+                    System.out.println("invalid answer");
+                    break;
+            }
+
+        }
+
+
+        System.out.println("La tua lista dei regali:");
+        for(String element : gift){
+            System.out.println(element.toUpperCase());
+        }
+
+        scanner.close();
+    }
+}
